3caad4b69b6f3f8cdd97b82f51579173aaa3b57e
Mak-43 <letsmailmak@gmail.com>
2022-02-16 03:32:34 +0600

    get input and set output on input field

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ document.getElementById('add').addEventListener('click',function(){
     li.innerText='Brand new item'
     const ul=document.getElementById('ul')
     ul.appendChild(li)
+    const input=document.getElementById('input')
+    let t=parseInt(input.value)+1
+    input.value=t
+    console.log(t)
 })
 
 
