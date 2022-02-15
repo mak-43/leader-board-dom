8ed01b683849f2f405f6fb35042bee49df875a3e
Mak-43 <letsmailmak@gmail.com>
2022-02-16 03:14:20 +0600

    practice

---
+++ app.js
@@ -0,0 +1,14 @@
+document.getElementById('top-players').style.color='red'
+document.getElementById('top-blogs').style.color='red'
+
+const p=document.getElementById('plyr').style.backgroundColor='rgba(0, 0, 255, 0.1)'
+
+document.getElementById('add').addEventListener('click',function(){
+    const li=document.createElement('li')
+    li.innerText='Brand new item'
+    const ul=document.getElementById('ul')
+    ul.appendChild(li)
+})
+
+
+
